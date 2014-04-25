432bbebbfa6376779bc605a45857ccdf5fef4b59
Ian Macalinao <ian@giza.us>
2014-04-25 17:53:13 -0500

    Make sound generator use common
    Signed-off-by: Ian Macalinao <ian@giza.us>

--- soundgen.py
+++ soundgen.py
@@ -1,15 +1,6 @@
 from wavebender import *
 import sys
-
-start = 10
-final = 200
-
-end_time= 10
-
-def val(i):
-    time = float(i) / 44100
-    k = (final - start) / end_time
-    return math.sin(2.0 * math.pi * (start * time + ((k * time * time) / 2)))
+from common import *
 
 def sweep():
     return (val(i) for i in count(0))
